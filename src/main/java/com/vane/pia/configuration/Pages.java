371a884aba3893ef146e328997e5197efe70f7ac
Ondřej Váně <onry.vane@gmail.com>
2019-11-30 12:33:35 +0100

    Style home page #4
    - new styles for index page add
    - add configuration for img directory

---
+++ src/main/java/com/vane/pia/configuration/Pages.java
@@ -0,0 +1,8 @@
+package com.vane.pia.configuration;
+
+public class Pages {
+
+    public static final String HOME_PAGE = "index";
+    private Pages() {
+    }
+}
--- src/main/java/com/vane/pia/web/controller/IndexController.java
+++ src/main/java/com/vane/pia/web/controller/IndexController.java
@@ -1,14 +1,15 @@
 package com.vane.pia.web.controller;
 
+import com.vane.pia.configuration.Pages;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String index() {
+        return Pages.HOME_PAGE;
+    }
 
 }
