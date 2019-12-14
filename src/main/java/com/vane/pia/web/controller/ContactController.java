35662e9f4b10a55e279e78c2e134883f19892eee
Ondřej Váně <onry.vane@gmail.com>
2019-12-15 00:13:37 +0100

    Contact managing #10
    - editing contacts

--- src/main/java/com/vane/pia/web/controller/ContactController.java
+++ src/main/java/com/vane/pia/web/controller/ContactController.java
@@ -4,17 +4,31 @@ import com.vane.pia.configuration.Pages;
 import com.vane.pia.domain.Contact;
 import com.vane.pia.service.ContactManager;
 import com.vane.pia.utils.LongParser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@Slf4j
 public class ContactController {
 
     @Autowired
     ContactManager contactManager;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
 
     @GetMapping(Pages.CONTACT_PAGE)
     public String showContactDetails(@PathVariable String contactId, Model model) {
@@ -23,6 +37,24 @@ public class ContactController {
         model.addAttribute("contact", contact);
 
         return Pages.EDIT_CONTACT_TEMPLATE;
+    }
+
+    @PostMapping(Pages.CONTACT_PAGE)
+    public String updateContact(@PathVariable String contactId,
+                                @Valid Contact contact,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            log.warn("BINDING RESULT ERROR");
+            contact.setId(LongParser.parseLong(contactId));
+            model.addAttribute("contact", contact);
+            return Pages.EDIT_CONTACT_TEMPLATE;
+        }
+
+        contact.setId(LongParser.parseLong(contactId));
+        contactManager.addContact(contact);
+
+        return "redirect:/contacts/" + contactId + "?success";
     }
 
 }
